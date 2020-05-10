00830a6a4eff2bcf05f7a881330240b9fd0cb502
Timur <timur@fralik.ru>
2020-05-10 23:22:20 +0300

    After 8 video

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+Vue.component('task-list', {
+    template: '<div><task v-for="task in tasks">{{ task.task }}</task></div>',
+
+    data() {
+        return {
+            tasks: [
+                { task: 'Task 11', complete: true },
+                { task: 'Task 12', complete: false },
+                { task: 'Task 13', complete: true },
+                { task: 'Task 14', complete: false },
+            ]
+        };
+    }
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
