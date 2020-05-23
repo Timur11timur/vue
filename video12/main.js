62f6e723b12b6902b2502b81631be9d28c1a2b30
Timur <t.fralik@aimprosoft.com>
2020-05-23 13:30:40 +0300

    After 12 video

--- video12/main.js
+++ video12/main.js
@@ -1,8 +1,26 @@
-Vue.component('modal', {
+Vue.component('coupon', {
+    template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
 
+    methods: {
+        onCouponApplied() {
+            alert('Applied!');
+            this.$emit('applied');
+        }
+    }
 
 });
 
 new Vue({
-    el: '#root'
+    el: '#root',
+
+    data: {
+        couponApplied: false
+    },
+
+    methods: {
+        onCouponApplied() {
+            alert('It was applied!');
+            this.couponApplied = true;
+        }
+    }
 });
