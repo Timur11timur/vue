1d26f5165e6b1b93fb6e0b2e41d05a57750aad38
Timur <t.fralik@aimprosoft.com>
2020-05-23 18:51:39 +0300

    After 14 video

--- video14/main.js
+++ video14/main.js
@@ -1,6 +1,25 @@
 Vue.component('modal', {
-
-
+    template: `
+        <div class="modal fade show" style="display: block;" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">
+                            <slot name="header"></slot>
+                        </h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <slot></slot>
+                    </div>
+                    <div class="modal-footer">
+                        <slot name="footer"></slot>
+                    </div>
+                </div>
+            </div>
+        </div>`
 });
 
 new Vue({
